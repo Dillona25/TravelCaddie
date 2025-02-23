0d60cf87981a6ebedfb67873ff7961349dbd9795
Dillona25 <Dillyb00by49@gmail.com>
2025-02-22 20:37:17 -0600

    Added storybook

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Navigation = () => {
-  return <div>navigation</div>;
+  return <div className="bg-black">navigation</div>;
 };
 
 export default Navigation;
---
+++ src/components/navigation/navigation.stories.jsx
@@ -0,0 +1,11 @@
+import Navigation from './navigation';
+
+const meta = {
+  component: Navigation,
+};
+
+export default meta;
+
+export const Default = {
+  args: {}
+};
