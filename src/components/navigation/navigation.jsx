082ff2cc265e34ef40ed381b0e16c4543f686911
Dillona25 <Dillyb00by49@gmail.com>
2025-02-26 22:18:15 -0600

    Status commit

---
+++ src/components/navigation/navigation.jsx
@@ -0,0 +1,10 @@
+export const Navigation = () => {
+  return (
+    <div className="py-2 px-4 d-flex justify-content-between">
+      <h2>LOGO</h2>
+      <div></div>
+    </div>
+  );
+};
+
+export default Navigation;
